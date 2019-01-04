014c944573ed103f0cdf3f58e3111e945ba97467
Dante S Bortone <dbortone@email.unc.edu>
2019-01-04 06:12:53 -0500

    Documentation.

---
+++ R/housekeeping.R
@@ -0,0 +1,10 @@
+#' housekeeping: Basic Debugging and Package Building Tasks
+#'
+#' The housekeeping package provides tools for debugging, script path finding and package maintanance.
+#' 
+#' @import devtools rstudioapi stringr
+#' @importFrom magrittr "%>%"
+#' 
+#' @keywords internal
+"_PACKAGE"
+#> [1] "_PACKAGE"
